cbc36e74250817d41dbeb52524fd273268cfe510
Alex <steepsanya@bigmir.net>
2018-03-06 20:07:43 +0200

    first commit

---
+++ src/8_MVVM/PersonsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace _8_MVVM
+{
+    class PersonsViewModel : DependencyObject
+    {
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(PersonsViewModel), new PropertyMetadata("", FilterText_Changed));
+
+        private static void FilterText_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var current = d as PersonsViewModel;
+            if (current != null)
+            {
+                current.Items.Filter = null;
+                current.Items.Filter = current.FilterPerson;
+            }
+        }
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(PersonsViewModel), new PropertyMetadata(null));
+
+        public PersonsViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Person.GetPersons());
+            Items.Filter = FilterPerson;
+        }
+
+        private bool FilterPerson(object obj)
+        {
+            bool result = true;
+            var person = obj as Person;
+            if(!string.IsNullOrEmpty(FilterText) && person != null && (!person.FirstName.Contains(FilterText) && !person.LastName.Contains(FilterText))) {
+                result = false;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ src/8_MVVM/Preson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_MVVM
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static List<Person> GetPersons()
+        {
+            return new List<Person>
+            {
+                new Person { FirstName = "Harry", LastName = "Potter" },
+                new Person { FirstName = "Ron", LastName = "Weasley" },
+                new Person { FirstName = "Hermione", LastName = "Granger" }
+            };
+        }
+    }
+}
